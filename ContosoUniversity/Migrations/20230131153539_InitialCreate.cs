08484fb87cd0b909e9fd17677cafaaf14073c845
Nikko Amante <84558605+amantenikko@users.noreply.github.com>
2023-02-01 22:50:49 +0800

    Add project files
    Contoso University

---
+++ ContosoUniversity/Migrations/20230131153539_InitialCreate.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Credits = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrollments",
+                columns: table => new
+                {
+                    EnrollmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseID = table.Column<int>(type: "int", nullable: false),
+                    StudentID = table.Column<int>(type: "int", nullable: false),
+                    Grade = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollments", x => x.EnrollmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstMidName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EnrollmentDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseEnrollment",
+                columns: table => new
+                {
+                    CourseID = table.Column<int>(type: "int", nullable: false),
+                    EnrollmentsEnrollmentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseEnrollment", x => new { x.CourseID, x.EnrollmentsEnrollmentID });
+                    table.ForeignKey(
+                        name: "FK_CourseEnrollment_Courses_CourseID",
+                        column: x => x.CourseID,
+                        principalTable: "Courses",
+                        principalColumn: "CourseID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseEnrollment_Enrollments_EnrollmentsEnrollmentID",
+                        column: x => x.EnrollmentsEnrollmentID,
+                        principalTable: "Enrollments",
+                        principalColumn: "EnrollmentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnrollmentStudent",
+                columns: table => new
+                {
+                    EnrollmentsEnrollmentID = table.Column<int>(type: "int", nullable: false),
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnrollmentStudent", x => new { x.EnrollmentsEnrollmentID, x.StudentID });
+                    table.ForeignKey(
+                        name: "FK_EnrollmentStudent_Enrollments_EnrollmentsEnrollmentID",
+                        column: x => x.EnrollmentsEnrollmentID,
+                        principalTable: "Enrollments",
+                        principalColumn: "EnrollmentID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EnrollmentStudent_Students_StudentID",
+                        column: x => x.StudentID,
+                        principalTable: "Students",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseEnrollment_EnrollmentsEnrollmentID",
+                table: "CourseEnrollment",
+                column: "EnrollmentsEnrollmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrollmentStudent_StudentID",
+                table: "EnrollmentStudent",
+                column: "StudentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseEnrollment");
+
+            migrationBuilder.DropTable(
+                name: "EnrollmentStudent");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Enrollments");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public ICollection<Course>? Course { get; set; }
+        public ICollection<Student>? Student { get; set; }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F,
+    }
+}
