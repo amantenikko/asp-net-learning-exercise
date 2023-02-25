5a6dcee110667fd78e7be4c53d0846ac2ca8ebab
Nikko Amante <84558605+amantenikko@users.noreply.github.com>
2023-02-25 12:42:19 +0800

    Fix school context non-nullable properties

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -5,15 +5,15 @@ namespace ContosoUniversity.Data
 {
     public class SchoolContext : DbContext
     {
-        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) {}
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
 
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Department> Departments { get; set; }
-        public DbSet<Instructor> Instructors { get; set; }
-        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
-        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<Course> Courses { get; set; } = default!;
+        public DbSet<Enrollment> Enrollments { get; set; } = default!;
+        public DbSet<Student> Students { get; set; } = default!;
+        public DbSet<Department> Departments { get; set; } = default!;
+        public DbSet<Instructor> Instructors { get; set; } = default!;
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; } = default!;
+        public DbSet<CourseAssignment> CourseAssignments { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
